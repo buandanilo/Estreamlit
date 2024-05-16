ea46724a34b6447016d543f2dfae661ff9083e52
buandanilo <149659633+buandanilo@users.noreply.github.com>
2024-05-16 16:04:03 +0800

    upload files

--- FPtest.py
+++ FPtest.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('best_FPmodel.hdf5')
+  model=load_model('best_FPmodel.h5')
   return model
 model=load_model()
 st.write("""
